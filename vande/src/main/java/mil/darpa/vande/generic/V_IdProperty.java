1138392f2c63516aa835814720cb09235682ecb1
djue <djue@phy6.net>
2014-08-07 12:13:33 -0400

    Made the IDProperty class comparable, so we can have sorted collections.

--- vande/src/main/java/mil/darpa/vande/generic/V_IdProperty.java
+++ vande/src/main/java/mil/darpa/vande/generic/V_IdProperty.java
@@ -4,7 +4,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 
 @XmlAccessorType(XmlAccessType.FIELD)
-public class V_IdProperty {
+public class V_IdProperty implements Comparable<V_IdProperty> {
 	private String idName;
 	private String idValue;
 
@@ -38,5 +38,12 @@ public class V_IdProperty {
 		return "V_IdProperty ["
 				+ (idName != null ? "idName=" + idName + ", " : "")
 				+ (idValue != null ? "idValue=" + idValue : "") + "]";
+	}
+
+	@Override
+	public int compareTo(V_IdProperty o) {
+		// Compare the name first, then the value if needed.
+		int cmpName = idName.compareTo(o.idName);
+		return (cmpName != 0 ? cmpName : idValue.compareTo(o.idValue));
 	}
 }
